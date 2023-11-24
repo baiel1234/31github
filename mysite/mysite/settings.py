ae45a855c352072f1b1f382d4c75521c82ea4231
baiel1234 <avtandilovbajel47gmail.com>
2023-11-24 23:23:06 +0300

    start create class world,race

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'world.apps.WorldConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
