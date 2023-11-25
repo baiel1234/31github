e210c054d46beceedeff714ab54742c85941441e
baiel1234 <avtandilovbajel47gmail.com>
2023-11-25 23:43:40 +0300

    more views

--- mysite/world/urls.py
+++ mysite/world/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:worldn_id>/", views.detail, name="detail"),
+    path("<int:worldn_id>/results/", views.results, name="results"),
+    path("<int:worldn_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/world/views.py
+++ mysite/world/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+def detail(request, world_id):
+    return HttpResponse("You're looking at world %s." % world_id)
+
+
+def results(request, world_id):
+    response = "You're looking at the results of world %s."
+    return HttpResponse(response % world_id)
+
+
+def vote(request, world_id):
+    return HttpResponse("You're voting on world %s." % world_id)
